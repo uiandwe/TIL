c184d24f609d05c9b9de8a4c5f3a1d0ecabea317
hyeon seung jae <uiandwe@gmail.com>
2018-10-18 14:45:05 +0900

    Create 4.bfs(linkedList).py

---
+++ algorithm/backjoon/24.graph/4.bfs(linkedList).py
@@ -0,0 +1,58 @@
+import queue
+
+a = []
+check = []
+path = []
+q = queue.Queue()
+
+
+for i in range(6+1):
+    a.append([])
+    check.append(0)
+
+
+for i in range(7*2):
+    str = raw_input()
+    str = str.split(' ')
+    node = str[0]
+    v = str[1]
+    w = str[2]
+
+    vector = [int(v), int(w)]
+    a[int(node)].append(vector)
+
+
+for i in a:
+    print i
+
+
+
+def bfs(x):
+
+
+    for i in range(len(a[x])):
+        y = a[x][i][0]
+        check[y] = 1
+        if y in path is False:
+            path.append(y)
+
+        if check[y] == 0:
+            dfs(y)
+
+
+q.push(1)
+check[1] = 1
+
+while q.empty() is False:
+    x = q.front()
+    q.pop()
+    print x
+
+    for i in range(len(a[x])):
+        next = a[x][i][0]
+        if check[next] == 0:
+            check[next] = 1
+            q.push(next)
+
+
+

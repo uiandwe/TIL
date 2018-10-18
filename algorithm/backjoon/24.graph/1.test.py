bc666aeeea6bab4933ef97af5b23561960c97ee5
hyeon seung jae <uiandwe@gmail.com>
2018-10-18 14:44:15 +0900

    Create 1.test.py

---
+++ algorithm/backjoon/24.graph/1.test.py
@@ -0,0 +1,42 @@
+
+
+# a = []
+#
+# for i in range(6+1):
+#     temp = []
+#     for j in range(6+1):
+#         temp.append(0)
+#     a.append(temp)
+#
+# for i in range(8):
+#     num1 = input()
+#     num2 = input()
+#
+#     a[num1][num2] = 1
+#     a[num2][num1] = 1
+#
+#
+# for i in a:
+#     print i
+
+
+
+# a = []
+#
+# for i in range(6+1):
+#     a.append([])
+#
+#
+# for i in range(8*2):
+#     str = raw_input()
+#     str = str.split(' ')
+#     node = str[0]
+#     v = str[1]
+#     w = str[2]
+#
+#     vector = [int(v), int(w)]
+#     a[int(node)].append(vector)
+#
+#
+# for i in a:
+#     print i

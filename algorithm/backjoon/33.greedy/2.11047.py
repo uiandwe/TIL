d8981efc6ecc6901a39c242f5f58a083fb9eb040
hyeon seung jae <uiandwe@gmail.com>
2018-10-22 17:02:04 +0900

    Create 2.11047.py

---
+++ algorithm/backjoon/33.greedy/2.11047.py
@@ -0,0 +1,18 @@
+# -*- coding: utf8 -*-
+import sys
+
+
+coin = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000]
+a = []
+d = [0]
+
+num1 = input()
+
+for i in range(num1+1):
+    d.append(sys.maxint)
+
+for i in coin:
+    for j in range(i, num1+1):
+        d[j] = min(d[j], d[j-i]+1)
+
+print d[num1]

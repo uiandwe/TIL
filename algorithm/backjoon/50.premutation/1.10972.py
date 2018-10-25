ae9e1c90aab3c6b8cf13b71d44cf8344841f63f3
hyeon seung jae <uiandwe@gmail.com>
2018-10-25 16:56:30 +0900

    Create 1.10972.py

---
+++ algorithm/backjoon/50.premutation/1.10972.py
@@ -0,0 +1,29 @@
+'''
+http://kwanghyuk.tistory.com/25
+'''
+
+num1 = input()
+
+def next_premutation(x):
+    str1 = str(x)
+    strLen = len(str1)
+    for i in range(strLen-1, 0, -1):
+        if int(str1[i]) > int(str1[i-1]):
+            tempArray1 = list(str1[:i])
+            tempArray2 = list(str1[i:])
+
+            tempArray1Len = len(tempArray1)
+            tempArray2Len = len(tempArray2)
+            templen = min(tempArray1Len, tempArray2Len)
+
+            for j in range(templen):
+                changeIntA = int(tempArray1[(tempArray1Len-1)-j])
+                changeIntB = int(tempArray2[(tempArray2Len-1)-j])
+                if changeIntA < changeIntB:
+                    tempArray1[(tempArray1Len-1)-j] = str(changeIntB)
+                    tempArray2[(tempArray2Len-1)-j] = str(changeIntA)
+                    tempArray2.sort()
+                    return ''.join(tempArray1)+''.join(tempArray2)
+
+
+print next_premutation(num1)

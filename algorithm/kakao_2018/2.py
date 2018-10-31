22e006c453929a9189e370037a653195cfc85575
hyeon seung jae <uiandwe@gmail.com>
2018-10-31 17:26:05 +0900

    Create 2.py

---
+++ algorithm/kakao_2018/2.py
@@ -0,0 +1,49 @@
+def solution(N, stages):
+    answer = []
+    test = []
+
+    for i in range(0, N+1):
+        test.append(0)
+
+    totalSuccess = 0
+
+    for s in stages:
+        if s == 1:
+            test[s] +=1
+        elif s > N:
+            totalSuccess += 1
+            test[s-1] += 1
+        else:
+            test[s] += 1
+
+    add = 0
+    sortObj = {}
+
+    for key in range(N, 0, -1):
+        success = 0
+        ing = 0
+
+        if key == N:
+            add += test[key]
+            success = totalSuccess
+            ing = test[key]
+        else:
+            success = add
+            add += test[key]
+            ing = add
+
+        failRate = success / ing
+
+        if failRate in sortObj:
+            sortObj[failRate].append(key)
+        else:
+            sortObj[failRate] = [key]
+
+    for i in sorted(sortObj.keys()):
+        answer += sorted(sortObj[i])
+
+    return answer
+
+
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(4, [4, 4, 4, 4, 4]))

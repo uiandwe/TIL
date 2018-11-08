5c6ce433567051d782f6ba89be8bd4c1c0e48967
hyeon seung jae <uiandwe@gmail.com>
2018-11-08 14:32:52 +0900

    Add files via upload

---
+++ Machine_Running/hunkim.github.io/6-1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import tensorflow as tf
+
+tf.set_random_seed(777)  # for reproducibility
+
+x_data = [[1, 2, 1, 1],
+          [2, 1, 3, 2],
+          [3, 1, 3, 4],
+          [4, 1, 5, 5],
+          [1, 7, 5, 5],
+          [1, 2, 5, 6],
+          [1, 6, 6, 6],
+          [1, 7, 7, 7]]
+y_data = [[0, 0, 1],
+          [0, 0, 1],
+          [0, 0, 1],
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 1, 0],
+          [1, 0, 0],
+          [1, 0, 0]]
+
+X = tf.placeholder("float", [None, 4])
+Y = tf.placeholder("float", [None, 3])
+nb_classes = 3
+
+W = tf.Variable(tf.random_normal([4, nb_classes]), name='weight')
+b = tf.Variable(tf.random_normal([nb_classes]), name='bias')
+
+hypothesis = tf.nn.softmax(tf.matmul(X, W)+b)
+
+cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.log(hypothesis), axis=1))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    
+    for step in range(2001):
+        sess.run(optimizer, feed_dict={X: x_data, Y:y_data})
+        if step % 200 == 0:
+            print(step, sess.run(cost, feed_dict={X: x_data, Y:y_data}))
+            
+            
+            
+            
+
+
+# In[ ]:
+
+
+
+

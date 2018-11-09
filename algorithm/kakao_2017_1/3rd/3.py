232320f5d9535741ddde0378a2dc198ffc5eacbe
hyeon seung jae <uiandwe@gmail.com>
2018-11-09 17:09:07 +0900

    Update 3.py

--- algorithm/kakao_2017_1/3rd/3.py
+++ algorithm/kakao_2017_1/3rd/3.py
@@ -2,8 +2,6 @@ dictionary = {}
 d = []
 str = ""
 
-
-
 def lzw():
     global dictionary
     global d
@@ -17,7 +15,7 @@ def lzw():
             findStr = tempStr
             break
 
-    if len(str) <= 1:
+    if len(str) <= 1 or findStr == "":
         d.append(dictionary[str])
         return True
 
@@ -40,9 +38,9 @@ def solution(msg):
 
 
 
-print(solution("KAKAO"))
-print([11, 1, 27, 15])
+# print(solution("KAKAO"))
+# print([11, 1, 27, 15])
 print(solution("TOBEORNOTTOBEORTOBEORNOT"))
 print([20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34])
-print(solution("ABABABABABABABAB"))
-print([1, 2, 27, 29, 28, 31, 30])
+# print(solution("ABABABABABABABAB"))
+# print([1, 2, 27, 29, 28, 31, 30])

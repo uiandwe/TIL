097cf49f489a8a4c3bd490380dd6654a0a54e1e4
hyeon seung jae <uiandwe@gmail.com>
2018-11-12 15:59:17 +0900

    Update binary_tree.py

--- algorithm/tree/binary_tree.py
+++ algorithm/tree/binary_tree.py
@@ -5,11 +5,16 @@ class Node:
 
 class tree():
     root = None
+    depth = 0
+    tempDepth = 0
 
     def insert(self, node, value):
         if node is None:
             node = Node(value)
+            if self.tempDepth > self.depth:
+                self.depth = self.tempDepth
         else:
+            self.tempDepth += 1
             if node.data > value:
                 node.left = self.insert(node.left, value)
             else:
@@ -18,6 +23,7 @@ class tree():
         return node
 
     def insertNode(self, value):
+        self.tempDepth = 0
         self.root = self.insert(self.root, value)
 
     def inOrder(self, node, array):
@@ -39,6 +45,7 @@ def solution(array):
     d = []
     t.inOrder(t.root, d)
     print(d)
+    print(t.depth)
 
 a = [2, 4, 1, 3, 9]
 

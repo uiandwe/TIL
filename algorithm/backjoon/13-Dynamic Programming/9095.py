1e52f9fed8bdb9b4c2c7bba3a361c6cdaf5b4a39
hyeon seung jae <uiandwe@gmail.com>
2018-11-30 01:19:42 +0900

    Create 9095.py

---
+++ algorithm/backjoon/13-Dynamic Programming/9095.py
@@ -0,0 +1,39 @@
+def solution(m):
+    d = [0, 1, 2, 4]
+
+    for i in range(4, m+1):
+        d.append(0)
+        d[i] = d[i-3]+d[i-2]+d[i-1]
+
+    print(d)
+
+solution(4)
+solution(7)
+solution(10)
+
+d= []
+
+def bt(a):
+    if d[a] > 0:
+        return d[a]
+
+    d[a] = bt(a-1) + bt(a-2) + bt(a-3)
+
+    return d[a]
+
+
+def solution(m):
+    global d
+    d = [0 for x in range(12)]
+
+    d[1] = 1
+    d[2] = 2
+    d[3] = 4
+    bt(m)
+
+    print(d)
+
+
+solution(4)
+solution(7)
+solution(10)

6a78d699ef8106a1309baddf350911cf4c58cc18
hyeon seung jae <uiandwe@gmail.com>
2018-12-04 15:01:26 +0900

    Update 53.py

--- algorithm/LC/53.py
+++ algorithm/LC/53.py
@@ -2,14 +2,12 @@ def solution(array):
     d = [0 for x in range(len(array))]
     d[0] = array[0]
 
+
     for i in range(1, len(array)):
-        d[i] = max(d[i-1] + array[i], array[i])
+        d[i] = max(array[i], array[i]+d[i-1])
 
-    maxInt = float('-inf')
-    for i in d:
-        maxInt = max(i, maxInt)
-
-    print(maxInt)
+    print(max(d))
 
 
 solution([-2,1,-3,4,-1,2,1,-5,4])
+solution([4, -1, 2, 1])

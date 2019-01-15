693997ea36aff536aa364fb276efa996dae68d1b
hyeon seung jae <uiandwe@gmail.com>
2019-01-15 23:27:16 +0900

    Update 10844.py

--- algorithm/backjoon/13-Dynamic Programming/acmicpc10844/10844.py
+++ algorithm/backjoon/13-Dynamic Programming/acmicpc10844/10844.py
@@ -1,1 +1,16 @@
 
+def solution(n):
+    d = [[0 for x in range(10)] for y in range(n+1)]
+    for i in range(1, 10):
+        d[1][i] = 1
+
+    for i in range(2, n+1):
+        for j in range(10):
+            d[i][j] = 0
+            if j - 1 >= 0:
+                d[i][j] += d[i-1][j-1]
+            if j + 1 <= 9:
+                d[i][j] += d[i-1][j+1]
+
+    return sum(d[n])
+

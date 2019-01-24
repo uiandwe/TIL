f87e2fb4ccd885af28de4ef75d41bf8b0e893992
hyeon seung jae <uiandwe@gmail.com>
2019-01-24 23:28:27 +0900

    Create test_1176.py

---
+++ algorithm/backjoon/37.etc/10809/acmicpc176/test_1176.py
@@ -0,0 +1,12 @@
+
+def test_1764():
+    names = [
+        "ohhenrie",
+        "charlie",
+        "baesangwook",
+        "obama",
+        "baesangwook",
+        "ohhenrie",
+        "clinton"
+    ]
+    assert 2 == solution(3, 4, names)

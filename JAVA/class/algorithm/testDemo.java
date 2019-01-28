a2092bcb941734957345a6aa8ade8415d4d68c6f
hyeon seung jae <uiandwe@gmail.com>
2019-01-28 19:17:07 +0900

    Update testDemo.java

--- JAVA/class/algorithm/testDemo.java
+++ JAVA/class/algorithm/testDemo.java
@@ -1,5 +1,8 @@
 package com.nts.algorithm;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 class Node{
 	int val = 0;
@@ -10,8 +13,18 @@ class Node{
 	}
 }
 
+class bNode{
+	int val = 0;
+	bNode left = null;
+	bNode right = null;
+	bNode(int data){
+		val = data;
+	}
+}
+
 class QuickSort1{
 	Node root = null;
+	bNode broot = null;
 	
 	public void sort(int[] nums, int left, int right) {
 		if(nums.length<=1) {
@@ -73,106 +86,38 @@ class QuickSort1{
 		root = createTree(root, data);
 	}
 	
-}
-
-public class testDemo {
-
-	public static void main(String[] args) {
-		QuickSort1 q = new QuickSort1();
-		int[] nums = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-		q.sort(nums, 0, nums.length-1);
-		for(int num:nums) {
-			System.out.print(num+"\t");
-		}
-		System.out.println("\n----------------");
-		System.out.println(q.search(nums, 1));
-
-		q.Tree(10);
-		System.out.println(q.root.val);
-		q.Tree(20);
-		System.out.println(q.root.next.val);
-		System.out.println(q.root.val);
-		q.Tree(30);
-		System.out.println(q.root.next.next.val);
-		
-	}
-
-}
-package com.nts.algorithm;
-
-
-class Node{
-	int val = 0;
-	Node next = null;
 	
-	Node(int data){
-		val = data;
-	}
-}
-
-class QuickSort1{
-	Node root = null;
-	
-	public void sort(int[] nums, int left, int right) {
-		if(nums.length<=1) {
-			return ;
-		}
-		
-		int i, j, temp, pivot;
-		i = left;
-		j = right;
-		
-		while(i<j) {
-			pivot = nums[(left+right)/2];
-			while(pivot < nums[j]) j--;
-			while(i<j && nums[i] < pivot) i++;
-			
-			temp = nums[i];
-			nums[i] = nums[j];
-			nums[j] = temp;
-			
-			sort(nums, left, i-1);
-			sort(nums, i+1, right);
-		}
-		
-	}
-	
-	
-	public int search(int[] nums, int data) {
-		int left = 0;
-		int right = nums.length-1;
-		
-		while(left <= right) {
-			int mid = (left+right)/2;
-			if(nums[mid] == data) {
-				return mid;
-			}
-			else if(nums[mid] < data) {
-				left = mid+1;
-			}
-			else {
-				right = mid-1;
-			}
-		}
-		
-		return -1;
-	}
-	
-	public Node createTree(Node n, int  data) {
+	public bNode createbTree(bNode n, int data) {
 		if(n == null) {
-			n = new Node(data);
+			n = new bNode(data);
 		}
 		else {
-			n.next = createTree(n.next, data);
+			if(n.val > data) {
+				n.left = createbTree(n.left, data);
+			}
+			else {
+				n.right = createbTree(n.right, data);
+			}
 		}
-		
 		return n;
 	}
 	
-	public void Tree(int data) {
-		root = createTree(root, data);
+	
+	public void bTree(int data) {
+		broot = createbTree(broot, data);
 	}
 	
+	
+	public void inOrder(bNode n, List<Integer> ret) {
+		if(n == null) {
+			return;
+		}
+		else {
+			if(n.left != null) inOrder(n.left, ret);
+			ret.add(n.val);
+			if(n.right != null) inOrder(n.right, ret);
+		}
+	}
 }
 
 public class testDemo {
@@ -194,7 +139,25 @@ public class testDemo {
 		System.out.println(q.root.val);
 		q.Tree(30);
 		System.out.println(q.root.next.next.val);
+		System.out.println("\n----------------");
 		
+		q.bTree(10);
+		System.out.println(q.broot.val);
+		q.bTree(20);
+		System.out.println(q.broot.val);
+		System.out.println(q.broot.right.val);
+		q.bTree(5);
+		q.bTree(1);
+		q.bTree(15);
+		q.bTree(50);
+		System.out.println(q.broot.left.val);
+		
+		List<Integer> al = new ArrayList();
+		q.inOrder(q.broot, al);
+		Iterator<Integer> it = al.iterator();
+		while(it.hasNext()) {
+			System.out.print(it.next()+"\t");
+		}
 	}
 
 }

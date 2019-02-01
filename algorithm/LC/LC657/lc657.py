de751de4ed7e4f1164000ad6f240b87e481e4790
hyeon seung jae <uiandwe@gmail.com>
2019-02-01 23:17:31 +0900

    Update lc657.py

--- algorithm/LC/LC657/lc657.py
+++ algorithm/LC/LC657/lc657.py
@@ -1,21 +1,6 @@
-class Solution(object):
-    def judgeCircle(self, moves):
-        """
-        :type moves: str
-        :rtype: bool
-        """
-        d = [0, 0]
-        for point in moves:
-            if point == "R":
-                d[0] += 1
-            elif point == "L":
-                d[0] -= 1
-            elif point == "U":
-                d[1] += 1
-            elif point == "D":
-                d[1] -= 1
-
-        if d[0] == 0 and d[1] == 0:
-            return True
-        else:
-            return False 
+def solution(moves):
+    if moves.count("U") == moves.count("D") \
+            and moves.count("L") == moves.count("R"):
+        return True
+    else:
+        return False

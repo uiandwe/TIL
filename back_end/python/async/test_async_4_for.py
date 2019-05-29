8b6586126e4db91dcbac5fd63ab28182ce786e30
hyeon seung jae <uiandwe@gmail.com>
2019-05-29 14:55:31 +0900

    Update test_async_4_for.py

--- back_end/python/async/test_async_4_for.py
+++ back_end/python/async/test_async_4_for.py
@@ -1,9 +1,12 @@
+# # -*- coding: utf-8 -*-
 import asyncio
+import random
 
 async def count():
     print("One")
     await asyncio.sleep(1)
     print("Two")
+    return random.randint(0, 10)
 
 async def main():
     res = await asyncio.gather(*(count() for i in range(3)))
@@ -13,10 +16,18 @@ if __name__ == "__main__":
     import time
     s = time.perf_counter()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    r1 = loop.run_until_complete(main())
+    print(r1)
+
     loop.close()
     elapsed = time.perf_counter() - s
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+
+
+
+
+
+
 
 
 import time

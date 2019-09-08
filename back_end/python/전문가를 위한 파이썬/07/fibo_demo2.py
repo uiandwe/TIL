1a637e7a515b4c1a29d160925f7c7382b61a74e9
hyeon seung jae <uiandwe@gmail.com>
2019-09-08 22:03:03 +0900

    Add files via upload

---
+++ back_end/python/전문가를 위한 파이썬/07/fibo_demo2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -8-
+
+import functools
+from clockdeco import clock
+import sys
+
+
+@functools.lru_cache()
+@clock
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-2) + fibonacci(n-1)
+
+
+if __name__ == '__main__':
+    print(fibonacci(6))
+

4d15c47fffdc121c43ecae055513a60814921ac4
hyeon seung jae <uiandwe@gmail.com>
2019-09-13 23:13:43 +0900

    Update 1463.py

--- algorithm/backjoon/13-Dynamic Programming/1463.py
+++ algorithm/backjoon/13-Dynamic Programming/1463.py
@@ -16,3 +16,32 @@ def solution(n):
 
 solution(2)
 solution(10)
+
+
+class MakeOne():
+    def __init__(self):
+        self.array = []
+
+    def __call__(self, *args, **kwargs):
+        value = args[0]
+        self.array = [0 for i in range(value + 1)]
+        self.array[2] = 1
+        self.array[3] = 1
+
+        if value <= 3:
+            return self.array[self.vlaue]
+
+        for i in range(4, value+1):
+            self.array[i] = self.array[i-1] + 1
+            if i % 3 == 0 and self.array[int(i/3)]+1 <= self.array[i]:
+                self.array[i] = self.array[int(i/3)] + 1
+            if i % 2 == 0 and self.array[int(i/2)]+1 <= self.array[i]:
+                self.array[i] = self.array[int(i/2)] + 1
+
+        print(self.array)
+
+
+mo = MakeOne()
+mo(4)
+mo(6)
+mo(10)

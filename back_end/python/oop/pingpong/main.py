662cc2b40ce8da974e752793213ce0cfccc2ef6c
hyeon seung jae <uiandwe@gmail.com>
2019-10-13 20:22:43 +0900

    Update main.py

--- back_end/python/oop/pingpong/main.py
+++ back_end/python/oop/pingpong/main.py
@@ -91,3 +91,14 @@ while True:
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
+    # paddle + ball
+    if (ball.xcor() > 340 and ball.xcor() < 350) and \
+            (ball.ycor() < paddle_b.ycor()+ 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and \
+            (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1

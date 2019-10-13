480d55187b900a104d07a41fd05c7fd517fbb41f
hyeon seung jae <uiandwe@gmail.com>
2019-10-13 15:04:30 +0900

    Create main.py

---
+++ back_end/python/oop/pingpong/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import turtle
+
+wn = turtle.Screen()
+wn.title("ping pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+# main game loop
+while True:
+    wn.update()

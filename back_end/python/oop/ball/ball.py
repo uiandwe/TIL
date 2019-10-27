4ef9f14dd6574eebf0196914ed2f1b0deaf5a05b
hyeon seung jae <uiandwe@gmail.com>
2019-10-27 19:55:56 +0900

    Update ball.py

--- back_end/python/oop/ball/ball.py
+++ back_end/python/oop/ball/ball.py
@@ -4,6 +4,7 @@ import turtle
 wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("ball simulator")
+wn.tracer(0)
 
 
 ball = turtle.Turtle()
@@ -13,13 +14,23 @@ ball.penup()
 ball.speed(0)
 ball.goto(0, 200)
 ball.dy = 0
+ball.dx = 2
 
 
 gravity = 0.1
 
 while True:
+    wn.update()
     ball.dy -= gravity
     ball.sety(ball.ycor() + ball.dy)
+    ball.setx(ball.xcor() + ball.dx)
+
+    # wall check
+    if ball.xcor() > 300:
+        ball.dx *= -1
+
+    if ball.xcor() < -300:
+        ball.dx *= -1
 
     # bounce
     if ball.ycor() < -300:

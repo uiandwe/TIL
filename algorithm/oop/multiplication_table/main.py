5ca45a65932e926e295f587f9b41930920a20b8d
hyeon seung jae <uiandwe@gmail.com>
2020-01-09 09:24:46 +0900

    Create main.py

---
+++ algorithm/oop/multiplication_table/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from oop.multiplication_table.controller.controler import MultiplicationTable
+
+if __name__ == '__main__':
+    multiplication_table = MultiplicationTable()
+    multiplication_table()

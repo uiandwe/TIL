8c71f92bc0ee3a31706644908da714f8b8f8b76b
hyeon seung jae <uiandwe@gmail.com>
2020-01-11 22:33:21 +0900

    Update turbo_plane.py

--- algorithm/oop/plane/turbo_plane.py
+++ algorithm/oop/plane/turbo_plane.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from oop.plane.turbo_imp import TurboImp
+from oop.plane.stealth_imp import StealthImp
+from oop.plane.plane import Plane
 
-class TurboPlane(TurboImp):
+
+class TurboPlane(Plane, TurboImp, StealthImp):
+
     def boost(self):
         print("가속")
+
+    def start_stealth(self):
+        print("안보임")
+
+    def stop_stealth(self):
+        print("보임")
+
+    def fly(self):
+        print("터보 날다")

1f75f8ae53f09cbb58d189ca92915249ca66d954
hyeon seung jae <uiandwe@gmail.com>
2020-01-11 22:32:48 +0900

    Update controller.py

--- algorithm/oop/plane/controller.py
+++ algorithm/oop/plane/controller.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     plane = Plane()
     plane.fly()
 
-
     turbo_plane = TurboPlane()
+    turbo_plane.fly()
     turbo_plane.boost()
+    turbo_plane.start_stealth()
+    turbo_plane.stop_stealth()

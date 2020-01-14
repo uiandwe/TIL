1935ad6a21d55e7280b9e204f2528f6b89cba305
hyeon seung jae <uiandwe@gmail.com>
2020-01-14 22:46:45 +0900

    Delete main.py

--- algorithm/oop/main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from oop.vending_machine.contoller.controller import Controller
-
-if __name__ == '__main__':
-    controller = Controller()
-    controller.make_products()
-    controller.show_products()
-    controller.insert_money(2000)
-    controller.select_product(0)

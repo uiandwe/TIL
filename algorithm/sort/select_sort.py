0d3718aa03026f3dc4cb3e3fd7aedd647d7e4fde
hyeon seung jae <uiandwe@gmail.com>
2020-01-27 22:07:34 +0900

    Update select_sort.py

--- algorithm/sort/select_sort.py
+++ algorithm/sort/select_sort.py
@@ -18,3 +18,21 @@ def solution(array):
 
 a = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9]
 print(solution(a))
+
+
+def select_sort(array):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i+1, len(array)):
+            if array[min_index] > array[j]:
+                min_index = j
+
+        if i != min_index:
+            array[i], array[min_index] = array[min_index], array[i]
+
+    return array
+
+
+assert select_sort([5, 1, 8, 2]) == [1, 2, 5, 8]
+assert select_sort([9,1,6,8,4,3,2,0]) == [0,1,2,3,4,6,8,9]
+

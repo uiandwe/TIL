a386f1bcb3a88f3c9a2999b408c8c3ad424190d6
hyeon seung jae <uiandwe@gmail.com>
2020-01-27 22:55:50 +0900

    Update quick_sort_python.py

--- algorithm/sort/quick_sort_python.py
+++ algorithm/sort/quick_sort_python.py
@@ -22,3 +22,28 @@ def q(a):
 a = [5, 4, 6, 100, 9, 8, 1, 24, 63, 12]
 
 print q(a)
+
+
+# -*- coding: utf-8 -*-
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = array[len(array)//2]
+    less = []
+    more = []
+    equal = []
+
+    for a in array:
+        if a < pivot:
+            less.append(a)
+        elif a > pivot:
+            more.append(a)
+        else:
+            equal.append(a)
+
+    return quick_sort(less) + equal + quick_sort(more)
+
+
+assert quick_sort([5, 1, 8, 2]) == [1, 2, 5, 8]
+assert quick_sort([9,1,6,8,4,3,2,0]) == [0,1,2,3,4,6,8,9]

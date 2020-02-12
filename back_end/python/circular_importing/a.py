7ea830647174d8ae225b31751849ad6ad1c0c09b
uiandwe <uiandwe@gmail.com>
2020-02-12 23:15:29 +0900

    add dynamic import

--- back_end/python/circular_importing/a.py
+++ back_end/python/circular_importing/a.py
@@ -1,8 +1,8 @@
 from b import B
 
 class A:
-    def __init__(self):
-        print("init A")
+    def __init__(self, params=0):
+        print("init A ", params)
 
     def test(self):
         B()
--- back_end/python/circular_importing/b.py
+++ back_end/python/circular_importing/b.py
@@ -3,11 +3,15 @@ import importlib
 class B:
     def __init__(self):
         a = importlib.import_module('a')
-        a.A()
+        a.A("1")
 
         import a
-        a.A()
+        a.A("2")
 
         a = __import__("a")
-        a.A()
+        a.A("3")
+
+        getattr(__import__('a'), 'A')("4")
+
+
         print("init B")

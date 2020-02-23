3fa2f04803907656aaf42962410d98119a3e7804
uiandwe <uiandwe@gmail.com>
2020-02-23 20:39:11 +0900

    object 1장 audience 캡슐화

--- algorithm/oop/theater/audience.py
+++ algorithm/oop/theater/audience.py
@@ -2,6 +2,11 @@ class Audience:
     def __init__(self, bag):
         self.__bag = bag
 
-    @property
-    def bag(self):
-        return self.__bag
+    def buy(self, ticket):
+        if self.__bag.has_invitation():
+            self.__bag.set_ticket(ticket)
+            return 0
+        else:
+            self.__bag.set_ticket(ticket)
+            self.__bag.minus_amount(ticket.fee())
+            return ticket.fee()
--- algorithm/oop/theater/ticket_seller.py
+++ algorithm/oop/theater/ticket_seller.py
@@ -3,12 +3,4 @@ class TicketSeller:
         self.__ticket_office = ticket_office
 
     def sell_to(self, audience):
-        if audience.bag().has_invitation():
-            ticket = self.__ticket_office.ticket()
-            audience.bag().set_ticket(ticket)
-
-        else:
-            ticket = self.__ticket_office.ticket()
-            audience.bag().miuns_amount(ticket.fee())
-            self.__ticket_office.plus_amount(ticket.fee())
-            audience.bag().set_ticket(ticket)
+        self.__ticket_office.plus_amount(audience.buy(self.__ticket_office.tickets()))

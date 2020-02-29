95e98e5978156a3b99d4ae46161276eb3c14d2cb
hyeon seung jae <uiandwe@gmail.com>
2020-02-29 23:26:19 +0900

    Add files via upload

---
+++ back_end/python/tkinter/basic/4_menu bar.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    #Creation of init_window
+    def init_window(self):
+
+        # changing the title of our master widget
+        self.master.title("GUI")
+
+        # allowing the widget to take the full space of the root window
+        self.pack(fill=BOTH, expand=1)
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        file = Menu(menu)
+        file.add_command(label='Exit', command=self.client_exit)
+        menu.add_cascade(label='File', menu=file)
+
+    def client_exit(self):
+        exit()
+
+root = Tk()
+
+#size of the window
+root.geometry("400x300")
+
+app = Window(root)
+root.mainloop()

9fa10d67c75e5255cbd13feaa0d84b65099823fe
hyeon seung jae <uiandwe@gmail.com>
2020-03-01 18:49:01 +0900

    Add files via upload

---
+++ back_end/python/stock/trading/5.graph_candle3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import pandas_datareader.data as web
+import datetime
+import matplotlib.pyplot as plt
+import mpl_finance
+import matplotlib.ticker as ticker
+
+start = datetime.datetime(2016, 3, 1)
+end = datetime.datetime(2016, 3, 31)
+skhynix = web.DataReader("000660.KS", "yahoo", start, end)
+#skhynix = skhynix[skhynix['Volume'] > 0]
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111)
+
+day_list = []
+name_list = []
+for i, day in enumerate(skhynix.index):
+    if day.dayofweek == 0:
+        day_list.append(i)
+        name_list.append(day.strftime('%Y-%m-%d') + '(Mon)')
+
+ax.xaxis.set_major_locator(ticker.FixedLocator(day_list))
+ax.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
+
+mpl_finance.candlestick2_ohlc(ax, skhynix['Open'], skhynix['High'], skhynix['Low'], skhynix['Close'], width=0.5, colorup='r', colordown='b')
+plt.grid()
+plt.show()

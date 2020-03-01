4d49d68a7ae202916d8845e8b676eac450d53573
hyeon seung jae <uiandwe@gmail.com>
2020-03-01 18:48:36 +0900

    Add files via upload

---
+++ back_end/python/stock/trading/5.graph_candle1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pandas_datareader.data as web
+import datetime
+import matplotlib.pyplot as plt
+import mpl_finance
+
+start = datetime.datetime(2016, 3, 1)
+end = datetime.datetime(2016, 3, 31)
+
+skhynix = web.DataReader("000660.KS", "yahoo", start, end)
+#print(skhynix)
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111)
+mpl_finance.candlestick2_ohlc(ax, skhynix['Open'], skhynix['High'], skhynix['Low'], skhynix['Close'],
+                         width=0.5, colorup='r', colordown='b')
+plt.show()

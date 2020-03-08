f4f5cefdc1ed5af68a103b05480c665d8b4522a7
hyeon seung jae <uiandwe@gmail.com>
2020-03-08 13:52:44 +0900

    Add files via upload

--- algorithm/oop/objects/reservation.py
+++ algorithm/oop/objects/reservation.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 class Reservation:
-    pass
+    def __init__(self, customer, screening, fee, audience_count):
+        self.customer = customer
+        self.screening = screening
+        self.fee = fee
+        self.audience_count = audience_count

85717f8256d05d906cbecc14ef2c3a0370480254
hyeon seung jae <uiandwe@gmail.com>
2020-03-08 13:51:17 +0900

    Add files via upload

--- algorithm/oop/objects/money.py
+++ algorithm/oop/objects/money.py
@@ -7,3 +7,18 @@ class Money:
     @staticmethod
     def wons(amount):
         return Money(amount)
+
+    def plus(self, amount):
+        return Money(self.amount + amount.amount)
+
+    def minus(self, amount):
+        return Money(self.amount - amount.amount)
+
+    def times(self, percent):
+        return Money(self.amount * percent)
+
+    def is_less_than(self, other):
+        return self.amount - other.amount < 0
+
+    def is_greather_than_or_equal(self, other):
+        return self.amount - other.amount >= 0

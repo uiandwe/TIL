a4c16fe4d8640506c7d636ccd1c5d6d4ced92cee
hyeon seung jae <uiandwe@gmail.com>
2020-03-08 13:59:21 +0900

    Add files via upload

---
+++ algorithm/oop/objects/movie.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+class Movie:
+    def __init__(self, title, running_time, fee, discount_policy):
+        self.title = title
+        self.running_time = running_time
+        self.fee = fee
+        self.discount_policy= discount_policy
+
+    def get_fee(self):
+        return self.fee
+
+    def calculate_movie_fee(self, screening):
+        return self.fee.minus(self.discount_policy.calculate_discount_amount(screening))

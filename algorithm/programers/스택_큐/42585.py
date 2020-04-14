68c595481d8160cfe35105832e0f21d57920797a
hyeon seung jae <uiandwe@gmail.com>
2020-04-14 14:38:53 +0900

    Update 42585.py

--- algorithm/programers/스택_큐/42585.py
+++ algorithm/programers/스택_큐/42585.py
@@ -1,52 +1,24 @@
-# https://programmers.co.kr/learn/courses/30/lessons/42585
-class Stack:
-    def __init__(self):
-        self.array = []
-
-    def push(self, value):
-        self.array.append(value)
-
-    def pop(self):
-        if self.empty():
-            return -1
-        else:
-            return self.array.pop()
-
-    def size(self):
-        return len(self.array)
-
-    def empty(self):
-        return True if self.size() <= 0 else False
-
-    def top(self):
-        if self.empty():
-            return False
-        else:
-            return self.array[-1]
-
-
 def solution(arrangement):
     answer = 0
-
-    s = Stack()
-
-    for index, c in enumerate(arrangement):
-        if c == ")":
-            s.pop()
-            if arrangement[index - 1] == "(":  # 레이저
-                answer += s.size()
-            else:  # 막대
-                answer += 1
+    s = []
+    for index, val in enumerate(arrangement):
+        if val == "(":
+            s.append("(")
+            answer += 1
         else:
-            s.push(c)
+            # 레이저
+            if arrangement[index-1] == "(":
+                answer += (len(s)-1) -1
+            s.pop()
 
     return answer
 
 
+assert solution("()(((()())(())()))(())") == 17
+assert solution("()") == 0
 assert solution("(())") == 2
+assert solution("((()))") == 4
+assert solution("(((())))") == 6
 assert solution("(()())") == 3
-assert solution("(())(())") == 4
-assert solution("((())(()))") == 7
-assert solution("((())(()))") == 7
-assert solution("((())()(()))") == 8
-assert solution("(())(()())") == 5
+assert solution("((()()))") == 6
+assert solution("(((()())))") == 9

e153f0dad63b13d66ad96f291d4ef181d9a3c656
hyeon seung jae <uiandwe@gmail.com>
2020-05-02 16:11:30 +0900

    Add files via upload

--- algorithm/LC/215.py
+++ algorithm/LC/215.py
@@ -1,7 +1,20 @@
-def solution(array, k):
-    array = sorted(array)
-    print(array[(-k)])
+# -*- coding: utf-8 -*-
+import heapq
 
 
-solution([3, 2, 1, 5, 6, 4], 2)
-solution([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+class Solution:
+    def findKthLargest(self, nums, k):
+        h = []
+        for i in nums:
+            heapq.heappush(h, i*-1)
+
+        for i in range(k-1):
+            heapq.heappop(h)
+        return heapq.heappop(h)*-1
+
+
+s = Solution()
+assert s.findKthLargest([3,2,1,5,6,4], 2) == 5
+s.findKthLargest([3,2,3,1,2,4,5,5,6], 4)
+s.findKthLargest([1], 1)
+s.findKthLargest([99, 99], 2)

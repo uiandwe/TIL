665889c47ef51f7ab19b05e1b8e5121c19f3c3d3
hyeon seung jae <uiandwe@gmail.com>
2020-06-13 03:10:52 +0900

    Add files via upload

---
+++ back_end/go/12.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(s, "***", i)
+	}
+}
+
+func main() {
+	// 함수를 동기적으로 실행
+	say("Sync")
+
+	// 함수를 비동기적으로 실행
+	go say("Async1")
+	go say("Async2")
+	go say("Async3")
+
+	// 3초 대기
+	time.Sleep(time.Second * 3)
+
+
+	// WaitGroup 생성. 2개의 Go루틴을 기다림.
+	var wait sync.WaitGroup
+	wait.Add(2)
+
+	// 익명함수를 사용한 goroutine
+	go func() {
+		defer wait.Done() //끝나면 .Done() 호출
+		fmt.Println("Hello")
+	}()
+
+	// 익명함수에 파라미터 전달
+	go func(msg string) {
+		defer wait.Done() //끝나면 .Done() 호출
+		fmt.Println(msg)
+	}("Hi")
+
+	wait.Wait() //Go루틴 모두 끝날 때까지 대기
+}

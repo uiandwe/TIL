fbd4bcf674d8fc2edac15495944b7d054c415840
hyeon seung jae <uiandwe@gmail.com>
2020-06-25 01:57:00 +0900

    Add files via upload

---
+++ back_end/go/30.go
@@ -0,0 +1,18 @@
+package main
+ 
+import (
+    "fmt"
+    "net/http"
+)
+ 
+func main() {
+    // "/" 경로로 접속했을 때 처리할 핸들러 함수 지정
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // "welcome!" 문자열을 화면에 출력
+        fmt.Fprintln(w, "welcome!")
+    })
+ 
+
+    // 8080 포트로 웹 서버 구동
+    http.ListenAndServe(":8080", nil)
+}

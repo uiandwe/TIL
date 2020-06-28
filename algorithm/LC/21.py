1456fff95be3c71023acc6233746e0905a6613ea
hyeon seung jae <uiandwe@gmail.com>
2020-06-28 14:48:29 +0900

    Add files via upload

--- algorithm/LC/21.py
+++ algorithm/LC/21.py
@@ -1,54 +1,44 @@
-class Node:
-    start = None
-    end = None
+# -*- coding: utf-8 -*-
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-    def insert(self, data):
-        n = Node()
-        n.data = data
-        n.next = None
-        if self.end is not None:
-            self.end.next = n
-            self.end = n
+class Solution:
+    root = None
+    tail = None
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        lh = l1
+        rh = l2
+        while lh and rh:
+            if lh.val <= rh.val:
+                self.addNode(lh)
+                lh = lh.next
+            else:
+                self.addNode(rh)
+                rh = rh.next
+
+        while lh:
+            self.addNode(lh)
+            lh = lh.next
+
+        while rh:
+            self.addNode(rh)
+            rh = rh.next
+
+        return self.root
+
+    def addNode(self, node):
+        if not self.root:
+            self.root = ListNode(node.val)
+            self.tail = self.root
         else:
-            self.end = self.start = n
+            self.tail.next = ListNode(node.val)
+            self.tail = self.tail.next
 
-    def insertArray(self, array):
-        for i in array:
-            self.insert(i)
-
-    def printNode(self):
-        n = self.start
-        array = []
-        while n is not None:
-            array.append(n.data)
-            n = n.next
-
-        return array
-
-def mergeLinkList(link1, link2):
-    point1 = link1.start
-    point2 = link2.start
-    temp = []
-    while point1 is not None or point2 is not None:
-        if point1 is None or (point2 is not None and point1.data >= point2.data):
-            temp.append(point2.data)
-            point2 = point2.next
-        elif point2 is None or (point1 is not None and point1.data <= point2.data):
-            temp.append(point1.data)
-            point1 = point1.next
-
-    return temp
-
-
-
-def solution(array1, array2):
-    print(array1, array2)
-    n1 = Node()
-    n1.insertArray(array1)
-
-    n2 = Node()
-    n2.insertArray(array2)
-
-    print(mergeLinkList(n1, n2))
-
-solution([1, 2, 4], [1, 3, 4])
+s = Solution()
+ln = ListNode(1, ListNode(2, ListNode(4)))
+rn = ListNode(1, ListNode(3, ListNode(4)))
+s.mergeTwoLists(ln, rn)

d7ce3e707c1864ff87ce5b6f766ac72d6099e014
hyeon seung jae <uiandwe@gmail.com>
2020-06-29 17:49:51 +0900

    Add files via upload

--- algorithm/LC/7.py
+++ algorithm/LC/7.py
@@ -3,33 +3,33 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        str_temp = str(x)
+        s = str(x)
+        if s[0] == '-':
+            s = int(s[1:][::-1]) * -1
+        else:
+            s = int(s[::-1])
 
-        sign = 1
-        if str_temp[0] == '-':
-            sign = -1
-            str_temp = str_temp[1:]
-
-        str_temp = str_temp[::-1]
-
-        answer = int(''.join(str_temp))
-        answer = sign * answer
-        if pow(2, 31) * -1 < answer < pow(2, 31)-1:
-            return answer
+        if pow(2, 31) * -1 < s < pow(2, 31) - 1:
+            return s
 
         return 0
 
 
-
-def test_solution():
-    s = Solution()
-    s.reverse("123")
-
-    assert s.reverse(123) == 321
-    assert s.reverse(-123) == -321
-    assert s.reverse(120) == 21
-    assert s.reverse(-1) == -1
-    assert s.reverse(1534236469) == 0
+#
+# def test_solution():
+#     s = Solution()
+#     s.reverse("123")
+#
+#     assert s.reverse(123) == 321
+#     assert s.reverse(-123) == -321
+#     assert s.reverse(120) == 21
+#     assert s.reverse(-1) == -1
+#     assert s.reverse(1534236469) == 0
 
 s = Solution()
 print(s.reverse(123))
+print(s.reverse(-123))
+print(s.reverse(-120))
+print(s.reverse(1534236469))
+
+

13a5341984d9a65a47cc975a8d656723698e2755
hyeon seung jae <uiandwe@gmail.com>
2020-07-01 00:02:04 +0900

    Add files via upload

--- back_end/python/oop/lotto/domain/lotto.py
+++ back_end/python/oop/lotto/domain/lotto.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from .lotto_machine import LottoMachine
-from .lotto_number import LottoNumber
 
 
 class Lotto:
@@ -15,18 +13,16 @@ class Lotto:
         self.lotto_bonus_number = None
 
     def create_numbers(self, lotto_machine):
-        return lotto_machine.create_numbers(LottoNumber.max_number)
+        raise NotImplementedError()
 
     def create_chain_number(self, lotto_machine):
-        return lotto_machine.create_chain_numbers(self.chain_number, self.lotto_numbers)
+        raise NotImplementedError()
 
     def create_bonus_number(self, lotto_machine):
-        return lotto_machine.create_bonus_numbers(self.chain_number, self.bonus_number, self.lotto_numbers)
+        raise NotImplementedError()
 
     def get_lotto_numbers(self):
-        self.lotto_numbers = self.create_numbers(LottoMachine())
-        self.lotto_chain_number = self.create_chain_number(LottoMachine())
-        self.lotto_bonus_number = self.create_bonus_number(LottoMachine())
+        raise NotImplementedError()
 
     def __str__(self):
         return " ".join([str(number) for number in self.lotto_chain_number])

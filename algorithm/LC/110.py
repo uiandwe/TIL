548b369335f240142fa22df75feb2997256f65ad
hyeon seung jae <uiandwe@gmail.com>
2020-07-11 00:49:58 +0900

    Add files via upload

---
+++ algorithm/LC/110.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isBalanced(self, root):
+
+        if not root:
+            return True
+        left_depth = self.depth(root.left)
+        right_depth = self.depth(root.right)
+        if abs(left_depth - right_depth) <= 1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else:
+            return False
+
+    def depth(self, node):
+        if not node:
+            return 0
+        return max(self.depth(node.left), self.depth(node.right)) + 1

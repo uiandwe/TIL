7a7ab3d81d94866d051bb423d5d84f615dd93e51
hyeon seung jae <uiandwe@gmail.com>
2020-07-17 05:56:10 +0900

    Add files via upload

--- algorithm/LC/2.py
+++ algorithm/LC/2.py
@@ -1,9 +1,41 @@
-def solution(array, target):
-    dict = {}
-    for i in range(len(array)):
-        if target-array[i] in dict:
-            return [dict[target-array[i]], i]
-        else:
-            dict[array[i]] = i
+# -*- coding: utf-8 -*-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        d = []
+        while l1 and l2:
+            d.append(l1.val + l2.val)
 
-print(solution([2, 7, 11, 15], 9))
+            l1 = l1.next
+            l2 = l2.next
+
+        while l1:
+            d.append(l1.val)
+            l1 = l1.next
+
+        while l2:
+            d.append(l2.val)
+            l2 = l2.next
+
+        root = result = None
+        round_up = 0
+        for i in range(len(d)):
+            if i == 0:
+                root = result = ListNode((d[i] + round_up) % 10)
+            else:
+                result.next = ListNode((d[i] + round_up) % 10)
+                result = result.next
+
+            if d[i] + round_up > 9:
+                round_up = 1
+            else:
+                round_up = 0
+
+        if round_up == 1:
+            result.next = ListNode(1)
+
+        return root

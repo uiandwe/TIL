bd36c6b158adbed8de971b68a70226361e0fdab6
hyeon seung jae <uiandwe@gmail.com>
2020-08-12 13:35:19 +0900

    Add files via upload

--- algorithm/LC/217.py
+++ algorithm/LC/217.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+import collections
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        d = collections.Counter(nums)
+        for key in d.keys():
+            if d[key] > 1:
+                return True
+
+        return False

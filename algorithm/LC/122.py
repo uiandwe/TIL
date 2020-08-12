30bfadc67626046ab5db06576ac230fd08b70d40
hyeon seung jae <uiandwe@gmail.com>
2020-08-12 11:17:32 +0900

    Add files via upload

--- algorithm/LC/122.py
+++ algorithm/LC/122.py
@@ -1,14 +1,18 @@
+# -*- coding: utf-8 -*-
+from typing import *
 class Solution:
-    def maxProfit(self, prices):
-        d = [0 for _ in range(len(prices))]
-
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price: int = prices[0]
+        ret_sum = 0
         for i in range(1, len(prices)):
-            if prices[i-1] < prices[i]:
-                d[i] = prices[i] - prices[i - 1]
+            if prices[i] > min_price:
+                ret_sum += prices[i] - min_price
+            min_price = prices[i]
+        return ret_sum
 
-        return sum(d)
+
 
 
 s = Solution()
-print(s.maxProfit([7,1,5,3,6,4]))
-print(s.maxProfit([1, 2, 3, 4, 5]))
+assert s.maxProfit([7,1,5,3,6,4]) == 7
+assert s.maxProfit([1, 2, 3, 4, 5]) == 4

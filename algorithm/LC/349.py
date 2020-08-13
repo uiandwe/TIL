813c0eac50f90c7ad18940618507855fced11ff6
hyeon seung jae <uiandwe@gmail.com>
2020-08-13 14:05:45 +0900

    Add files via upload

---
+++ algorithm/LC/349.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from typing import *
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1) & set(nums2)
+
+

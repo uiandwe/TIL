47878195ab64fc5e006dfcf9c1744f1644caadd5
hyeon seung jae <uiandwe@gmail.com>
2020-08-14 12:17:52 +0900

    Add files via upload

--- algorithm/LC/242.py
+++ algorithm/LC/242.py
@@ -1,5 +1,19 @@
-from collections import Counter
+# -*- coding: utf-8 -*-
+import collections
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
-            
+        if len(s) != len(t):
+            return False
+
+        d1 = collections.Counter(s)
+        d2 = collections.Counter(t)
+
+        for key in d1.keys():
+            if key not in d2.keys():
+                return False
+            if d2[key] != d1[key]:
+                return False
+
+        return True

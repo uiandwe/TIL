22926ba2661d643b4df36dd44b0272cfedf93f8b
hyeon seung jae <uiandwe@gmail.com>
2020-08-19 16:20:18 +0900

    Add files via upload

--- algorithm/LC/206.py
+++ algorithm/LC/206.py
@@ -1,39 +1,16 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
+# -*- coding: utf-8 -*-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def printList(self, head):
-        while head is not None:
-            print(head.val)
-            head = head.next
-
-    def reverseList(self, head):
-        new_head = None
+    def reverseList(self, head: ListNode) -> ListNode:
+        d = None
+        h = None
         while head:
-            p = head
+            h = head
             head = head.next
-            p.next = new_head
-            new_head = p
-
-        return new_head
-
-s = Solution()
-root = ln = None
-
-for i in range(5):
-    temp_ln = ListNode(i)
-    if root is None:
-        root = ln = temp_ln
-    else:
-        while ln.next is not None:
-            ln = ln.next
-        ln.next = temp_ln
-
-s.printList(root)
-
-root = s.reverseList(root)
-
-s.printList(root)
+            h.next = d
+            d = h
+        return d

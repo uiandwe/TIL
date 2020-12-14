b7f74c0e64e33a4b79d5de6af02bf641b7b1f949
hyeon seung jae <uiandwe@gmail.com>
2020-12-14 22:06:50 +0900

    Add files via upload

---
+++ algorithm/alogorithm_test/4_most_common_word.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import re
+import collections
+def mostCommonWord(paragraph: str, banned: List[str]) -> str:
+    words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+    counts = collections.Counter(words)
+    return counts.most_common(1)[0][0]

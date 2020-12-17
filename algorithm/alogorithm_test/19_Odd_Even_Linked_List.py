7f18f0fff04e59dc178901a538dbb60ae7e564c3
hyeon seung jae <uiandwe@gmail.com>
2020-12-17 23:55:02 +0900

    Add files via upload

---
+++ algorithm/alogorithm_test/19_Odd_Even_Linked_List.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+
+        if head is None:
+            return None
+
+        odd = head
+        even = head.next
+        even_head = head.next
+
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+
+        odd.next = even_head
+
+        return head

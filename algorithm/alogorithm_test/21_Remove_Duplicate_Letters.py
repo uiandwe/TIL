282bcb0022971c7de5119e1f0c7cf72f57de5a1b
hyeon seung jae <uiandwe@gmail.com>
2020-12-18 15:58:52 +0900

    Add files via upload

---
+++ algorithm/alogorithm_test/21_Remove_Duplicate_Letters.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import collections
+
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        counter, seen, stack = collections.Counter(s), set(), []
+
+
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+
+            stack.append(char)
+            seen.add(char)
+
+        return ''.join(stack)
+
+
+s = Solution()
+s.removeDuplicateLetters("bcacdcbc")

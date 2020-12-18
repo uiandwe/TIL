6bb99d86f3ed23ade043298b704745209d6b4a35
hyeon seung jae <uiandwe@gmail.com>
2020-12-18 12:19:54 +0900

    Add files via upload

---
+++ algorithm/alogorithm_test/20_Valid_Parentheses.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+
+        table = {
+            ')': '(',
+            ']': '[',
+            '}': '{',
+        }
+
+        for char in s:
+            if char not in table:
+                stack.append(char)
+            elif not stack or table[char] != stack.pop():
+                return False
+        return len(stack) == 0

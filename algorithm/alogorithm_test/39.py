982a41406be3988b466faff3031bd7b3174fced3
hyeon seung jae <uiandwe@gmail.com>
2020-12-26 17:09:39 +0900

    Add files via upload

---
+++ algorithm/alogorithm_test/39.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import collections
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites):
+        graph = collections.defaultdict(list)
+        for x, y in prerequisites:
+            graph[x].append(y)
+
+        traced = set()
+        visited = set()
+
+        def dfs(i):
+            if i in traced:
+                return False
+            if i in visited:
+                return True
+
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+
+            traced.remove(i)
+            visited.add(i)
+
+            return True
+
+        for x in list(graph):
+            if not dfs(x):
+                return False
+
+        return True
+
+s = Solution()
+print(s.canFinish(2, [[1,0],[2,0],[0,3],[0,4]]))

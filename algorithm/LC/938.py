41945a8f57f7f81a96dcd5fa1c259a24d906d99a
hyeon seung jae <uiandwe@gmail.com>
2020-12-28 17:27:25 +0900

    Add files via upload

---
+++ algorithm/LC/938.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        stack, sum = [root], 0
+
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val > low:
+                    stack.append(node.left)
+                if node.val < high:
+                    stack.append(node.right)
+
+                if low <= node.val <= high:
+                    sum += node.val
+
+        return sum

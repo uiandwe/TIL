54e05f0a6fd235089e612e563e27d5fd5551ddf7
hyeon seung jae <uiandwe@gmail.com>
2020-12-29 14:00:52 +0900

    Add files via upload

---
+++ algorithm/LC/231.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        ret = False
+        while n:
+            temp = n & 1
+            if ret and temp:
+                return False
+
+            if temp:
+                ret = True
+            n = n >> 1
+
+        return True
+
+
+
+s = Solution()
+assert s.isPowerOfTwo(16) == True
+assert s.isPowerOfTwo(4) == True
+assert s.isPowerOfTwo(3) == False
+assert s.isPowerOfTwo(5) == False

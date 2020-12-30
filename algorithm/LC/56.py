312b013133f6c360c457defaa51d3d4ff69a9932
hyeon seung jae <uiandwe@gmail.com>
2020-12-30 16:31:43 +0900

    Add files via upload

--- algorithm/LC/56.py
+++ algorithm/LC/56.py
@@ -1,22 +1,20 @@
-def solution(array):
+# -*- coding: utf-8 -*-
+class Solution:
+    def merge(self, intervals):
+        merged = []
+        for i in sorted(intervals, key=lambda x: x[0]):
 
-    array.sort(key=lambda x: x[0])
-
-    d = []
-    for a in range(1, len(array)):
-        if array[a-1][1] >= array[a][0]:
-            array[a][0] = array[a - 1][0]
-            if array[a-1][1] > array[a][1]:
-                array[a][1] = array[a-1][1]
-        else:
-            d.append(array[a-1])
-
-    d.append(array[-1])
-
-    print(d)
+            if merged and i[0] <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], i[1])
+            else:
+                merged.append(i)
+        return merged
 
 
 
 
-solution([[1, 3], [2, 6], [8, 10], [15, 18]])
-solution([[1, 4], [4, 6], [2, 3]])
+s = Solution()
+# s.merge([[1,3]])
+s.merge([[1,3],[2,6],[8,10],[15,18]])
+# s.merge([[1,4],[4,6]])
+# s.merge([[1,4],[5,6]])

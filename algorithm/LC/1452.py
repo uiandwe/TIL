c3757f0fbe4b9084bc0afad5ba3fb49802fb3487
hyeon seung jae <uiandwe@gmail.com>
2021-01-13 22:38:57 +0900

    Add files via upload

--- algorithm/LC/1452.py
+++ algorithm/LC/1452.py
@@ -5,23 +5,36 @@ from typing import *
 
 class Solution:
     def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
-        hm = {}
+        # hm = {}
+        # for i in range(len(favoriteCompanies)):
+        #     for j in favoriteCompanies[i]:
+        #         if j in hm:
+        #             hm[j].append(i)
+        #         else:
+        #             hm[j] = [i]
+        # ans = []
+        # for i in range(len(favoriteCompanies)):
+        #     b = set(hm[favoriteCompanies[i][0]])
+        #     for j in favoriteCompanies[i][1:]:
+        #         b = b & set(hm[j])
+        #     if len(b) == 1:
+        #         ans.append(i)
+        # return ans
+
+        output = []
         for i in range(len(favoriteCompanies)):
-            for j in favoriteCompanies[i]:
-                if j in hm:
-                    hm[j].append(i)
-                else:
-                    hm[j] = [i]
-        ans = []
-        for i in range(len(favoriteCompanies)):
-            b = set(hm[favoriteCompanies[i][0]])
-            for j in favoriteCompanies[i][1:]:
-                b = b & set(hm[j])
-            if len(b) == 1:
-                ans.append(i)
-        return ans
+            j = 0
+            answer = True
+            while j < len(favoriteCompanies):
+                if i != j and len(set(favoriteCompanies[i]) - set(favoriteCompanies[j])) == 0:
+                    answer = False
+                    break
+                j += 1
+            if answer:
+                output.append(i)
+        return output
 
 s = Solution()
-s.peopleIndexes([[ "leetcode", "facebook"], [ "leetcode", "google", "facebook"]]) == [0, 1, 4]
+s.peopleIndexes([["leetcode","google","facebook"],["google","microsoft"],["google","facebook"],["google"],["amazon"]]) == [0, 1, 4]
 
 

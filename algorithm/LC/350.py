26dd4f73b04d023eb40aec56635733596804cb1e
hyeon seung jae <uiandwe@gmail.com>
2021-01-14 15:54:40 +0900

    Add files via upload

--- algorithm/LC/350.py
+++ algorithm/LC/350.py
@@ -1,20 +1,18 @@
-import collections
-
+from collections import Counter
+from typing import *
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        r = []
-        nums1.sort()
-        nums2.sort()
-        i = j = 0
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                i += 1
-            elif nums1[i] > nums2[j]:
-                j += 1
-            else:
-                r.append(nums1[i])
-                i += 1
-                j += 1
+        d1 = Counter(nums1)
+        d2 = Counter(nums2)
+        intersection = d1 & d2
 
-        return r
+        return [key for key, val in intersection.items() for i in range(val)]
+
+
+
+
+
+s = Solution()
+assert s.intersect(nums1 = [1,2,2,1], nums2 = [2,2]) == [2, 2]
+assert s.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4]) == [4, 9]

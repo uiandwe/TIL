6af8e65e6ee326f09e4340ace7a92614327c77f3
hyeon seung jae <uiandwe@gmail.com>
2021-01-16 13:50:57 +0900

    Add files via upload

---
+++ algorithm/LC/88.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from typing import *
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(n):
+            nums1[i+m] = nums2[i]
+        nums1.sort()
+        print(nums1)
+
+s = Solution()
+s.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3) == [1,2,2,3,5,6]
+s.merge([1,2,4,5,6,0], 5, [3], 1)

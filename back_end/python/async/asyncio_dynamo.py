83f4bc7c9b4c1038e61828cf4812ec819bff8ede
hyeon seung jae <uiandwe@gmail.com>
2021-01-23 16:24:58 +0900

    Add files via upload

--- back_end/python/async/asyncio_dynamo.py
+++ back_end/python/async/asyncio_dynamo.py
@@ -3,23 +3,13 @@
 import asyncio
 import aioboto3
 
+d = [i for i in range(2)]
 
-from boto3.dynamodb.conditions import Key
-
-
-async def main():
+async def fetch(d):
     async with aioboto3.resource('dynamodb', region_name='ap-northeast-2') as dynamo_resource:
         table = await dynamo_resource.Table('test.ws-record')
-
-        await table.put_item(
-            Item={'key': 'test1', 'ref_dt': '20210123', 'option': 'some_data'}
-        )
-
-        result = await table.query(
-            KeyConditionExpression=Key('key').eq('test1')
-        )
-
-        print(result['Items'])
+        for data in d:
+            await table.put_item(Item={'key': 'test'+str(data), 'ref_dt': '20210123', 'option': data})
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(fetch(d))

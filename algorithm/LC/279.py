6d9f15c3fb27cdbc0bdfd46408969d9b5ea7ab59
hyeon seung jae <uiandwe@gmail.com>
2021-01-27 22:13:46 +0900

    Add files via upload

---
+++ algorithm/LC/279.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [i for i in range(n + 1)]
+
+        for i in range(n + 1):
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], dp[i - (j * j)] + 1)
+                j += 1
+
+        return dp[-1]
+s = Solution()
+assert s.numSquares(12) == 3

9a937e218a3baac946cfa2b6d98e27bc7a98a5d6
hyeon seung jae <uiandwe@gmail.com>
2021-03-02 09:19:57 +0900

    Add files via upload

--- algorithm/LC/2.py
+++ algorithm/LC/2.py
@@ -5,37 +5,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        d = []
-        while l1 and l2:
-            d.append(l1.val + l2.val)
+        root = head = ListNode()
 
-            l1 = l1.next
-            l2 = l2.next
+        carry = 0
+        while l1 or l2 or carry:
+            l1_val = 0
+            l2_val = 0
 
-        while l1:
-            d.append(l1.val)
-            l1 = l1.next
+            if l1:
+                l1_val = l1.val
+                l1 = l1.next
+            if l2:
+                l2_val = l2.val
+                l2 = l2.next
 
-        while l2:
-            d.append(l2.val)
-            l2 = l2.next
+            carry, val = divmod(l1_val + l2_val + carry, 10)
 
-        root = result = None
-        round_up = 0
-        for i in range(len(d)):
-            if i == 0:
-                root = result = ListNode((d[i] + round_up) % 10)
-            else:
-                result.next = ListNode((d[i] + round_up) % 10)
-                result = result.next
+            head.next = ListNode(val)
+            head = head.next
 
-            if d[i] + round_up > 9:
-                round_up = 1
-            else:
-                round_up = 0
-
-        if round_up == 1:
-            result.next = ListNode(1)
-
-        return root
+        return root.next

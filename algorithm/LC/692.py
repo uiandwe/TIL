3d426afbedd1715c2e5f45eeb3549ded2ac9ec0f
hyeon seung jae <uiandwe@gmail.com>
2021-03-18 21:19:14 +0900

    Add files via upload

--- algorithm/LC/692.py
+++ algorithm/LC/692.py
@@ -1,21 +1,3 @@
-
-def solution(array, k):
-    dict = {}
-    for str in array:
-        if str in dict:
-            dict[str] += 1
-        else:
-            dict[str] = 1
-
-    temp = sorted(dict, key=dict.get, reverse=True)[:k]
-    print(temp)
-
-
-
-solution(["i", "love", "leetcode", "i", "love", "coding"], 2)
-solution(["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], 4)
-
-
 # -*- coding: utf-8 -*-
 from collections import Counter
 from operator import itemgetter
@@ -29,7 +11,6 @@ class Solution:
 
         count_array.sort(key=itemgetter(0))
         count_array.sort(key=itemgetter(1), reverse=True)
-
         ret_arr = []
         for i in range(k):
             ret_arr.append(count_array[i][0])

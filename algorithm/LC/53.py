fbc79b8952a4e13e8a6bcf9eefde33ab52828b7b
hyeon seung jae <uiandwe@gmail.com>
2021-03-18 21:18:40 +0900

    Add files via upload

--- algorithm/LC/53.py
+++ algorithm/LC/53.py
@@ -4,11 +4,11 @@ class Solution:
         d = [0 for x in nums]
         d[0] = nums[0]
         for i in range(1, len(nums)):
-            d[i] = max(d[i-1]+nums[i], nums[i])
+            d[i] = max(nums[i], d[i-1]+nums[i])
 
         return max(d)
 
 s = Solution()
-print(s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
-print(s.maxSubArray([2, -1, 10]))
-print(s.maxSubArray([-2, -1]))
+assert s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6
+assert s.maxSubArray([2, -1, 10]) == 11
+assert s.maxSubArray([-2, -1]) == -1

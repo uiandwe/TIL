0c821560685e65d2d41ab26266f63ced2e29c7f3
hyeon seung jae <uiandwe@gmail.com>
2021-03-25 23:47:04 +0900

    Add files via upload

---
+++ algorithm/이것이코딩테스다/a01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+n = int(input())
+data = list(map(int, input().split()))
+
+data.sort()
+
+count = 0
+result = 0
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)

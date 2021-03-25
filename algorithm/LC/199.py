55a11f59c4b4b5ba3e1cf93ae30c24f6586e0470
hyeon seung jae <uiandwe@gmail.com>
2021-03-25 11:57:58 +0900

    Add files via upload

--- algorithm/LC/199.py
+++ algorithm/LC/199.py
@@ -1,80 +1,28 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = self.right = None
+# -*- coding: utf-8 -*-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
 
-class Tree:
-    def __init__(self):
-        self.root = None
+        d = defaultdict(list)
+        result = []
 
-    def push(self, node, data):
-        if node is None:
-            node = Node(data)
-        else:
-            if node.data > data:
-                node.left = self.push(node.left, data)
-            else:
-                node.right = self.push(node.right, data)
+        def dfs(node, depth):
+            if node:
+                d[depth].append(node.val)
+                if node.left:
+                    dfs(node.left, depth + 1)
+                if node.right:
+                    dfs(node.right, depth + 1)
 
-        return node
+        dfs(root, 0)
 
-    def pushNode(self, data):
-        self.root = self.push(self.root, data)
+        for k in d.keys():
+            result.append(d[k][-1])
 
-
-class Queue:
-    def __init__(self):
-        self.array = []
-
-    def push(self, data):
-        self.array.append(data)
-
-    def pop(self):
-        if self.empty() is False:
-            return self.array.pop(0)
-        else:
-            return False
-
-    def empty(self):
-        if self.size() > 0:
-            return False
-        else:
-            return True
-
-    def size(self):
-        return len(self.array)
-
-    def printArray(self):
-        temp = []
-        for i in range(len(self.array)):
-            temp.append(self.array[i])
-
-        print(','.join([str(x.data) for x in self.array]))
-
-
-def whileQueue(t, array):
-    q = Queue()
-    q.push(t.root)
-
-    while q.empty()is False:
-        array.append(q.array[q.size()-1].data)
-        for i in range(q.size()):
-            node = q.pop()
-            if node.left is not None:
-                q.push(node.left)
-            if node.right is not None:
-                q.push(node.right)
-
-
-def solution(array):
-    t = Tree()
-    for i in array:
-        t.pushNode(i)
-
-    printArray = []
-
-    whileQueue(t, printArray)
-    print(printArray)
-
-
-solution([4, 3, 1, 2, 8, 5, 9])
+        # print(result)
+        return result

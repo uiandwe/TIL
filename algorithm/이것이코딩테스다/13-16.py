e33a8dbc78b2c0ee3985df053eb8984e8d0d1c69
hyeon seung jae <uiandwe@gmail.com>
2021-04-01 23:52:36 +0900

    Add files via upload

---
+++ algorithm/이것이코딩테스다/13-16.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# n, m = list(map(int, input().split()))
+# data = []
+
+#
+# for i in range(n):
+#     data.append(list(map(int, input().split())))
+
+from codetiming import Timer
+
+n, m = 7, 7
+temp = [[0]* m for _ in range(n)]
+data = [
+    [2, 0, 0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 1, 2, 0],
+    [0, 1, 1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0]
+]
+
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+result = 0
+
+
+
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+                virus(nx, ny)
+
+def get_score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+
+
+def dfs(count):
+    global result
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
+
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
+
+        result = max(result, get_score())
+        return
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                dfs(count)
+                data[i][j] = 0
+                count -= 1
+
+with Timer(name="context manager"):
+    dfs(0)
+print(result)
+
+"""
+7 7
+2 0 0 0 1 1 0
+0 0 1 0 1 2 0
+0 1 1 0 1 0 0
+0 1 0 0 0 0 0 
+0 0 0 0 0 1 1
+0 1 0 0 0 0 0
+0 1 0 0 0 0 0
+>> 27
+
+"""

7331278bd835936f11314f7da16d06f97d7ed25e
hyeon seung jae <uiandwe@gmail.com>
2021-05-31 08:51:58 +0900

    Add files via upload

--- algorithm/LC/34.py
+++ algorithm/LC/34.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
+from typing import List
 class Solution:
-    def find_search(self, nums, target, t):
-        l = 0
-        h = len(nums)
-        while l < h:
-            mid = (l + h) // 2
-            temp = nums[mid]
-            if temp > target or (temp == target and t):
-                h = mid
-            else:
-                l = mid + 1
-        return l
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ret = [-1, -1]
 
-    def searchRange(self, nums, target: int):
-        if len(nums) <= 0:
-            return [-1, -1]
+        def find_bs(target, p):
+            index = -1
+            l, r = 0, len(nums)-1
+            while l <= r:
+                mid = l + (r - l) // 2
+                if nums[mid] == target:
+                    index = mid
+                    if p > 0:
+                        l = mid + p
+                    else:
+                        r = mid + p
+                elif nums[mid] > target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return index
 
-        left = self.find_search(nums, target, True)
+        ret[1] = find_bs(target, 1)
+        ret[0] = find_bs(target, -1)
 
-        if left >= len(nums) or nums[left] != target:
-            return [-1, -1]
+        return ret
 
-        right = self.find_search(nums, target, False) -1
-
-        return [left, right]
 
 
 s = Solution()
-print(s.searchRange([5,7,7,8,8,10], 8))
-print(s.searchRange([5,7,7,8,8,10], 6))
-print(s.searchRange([1], 1))
-print(s.searchRange([2, 2], 2))
+assert s.searchRange([5,7,7,8,8,10], 8) == [3, 4]
+assert s.searchRange([5,7,7,8,8,10], 6) == [-1, -1]
+assert s.searchRange([1], 1) == [0, 0]
+assert s.searchRange([2, 2], 2) == [0, 1]
 print(s.searchRange([1, 1, 2], 1))
 print(s.searchRange([], 0))
 print(s.searchRange([2, 2], 3))
